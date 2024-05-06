1cf72b9f405e1537cb76204c070690b69cffe99f
rahul bora <rahul.bora@yesmadam.com>
2024-05-06 11:50:11 +0530

    added changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,117 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Map from "./components/map/Map";
+import axios from "axios";
 
 function App() {
+  const center = {
+    lat: 25.2048, //! Default Dubai Lat long
+    lng: 55.2708,
+  };
+
+  const [branches, setBranches] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredMarkers, setFilteredMarkers] = useState([]);
+  const [mapCenter, setMapCenter] = useState(center);
+
+  const getCompanyLocations = (id) => {
+    axios
+      .get(`https://fcssoftwares.com/api/CompanyProfile/GetOfficeByCPID/${id}`)
+      .then((result) => {
+        console.log(result?.data);
+        setBranches(result?.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    getCompanyLocations(1);
+  }, []);
+
+  const data = branches && branches?.map((item) => item?.offices);
+  const markersData = data && data?.flatMap((item) => item);
+
+  const markers =
+    markersData &&
+    markersData?.map((branch, index) => ({
+      id: index,
+      name: branch?.branches?.address,
+      position: {
+        lat: parseFloat(branch?.branches?.lat),
+        lng: parseFloat(branch?.branches?.long),
+      },
+      description: branch.address,
+      country: branch?.branches?.address,
+      email: branch?.branches?.email,
+    }));
+
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const handleSearch = (event) => {
+    const filtered = markers.filter((marker) =>
+      marker.country.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    setFilteredMarkers(filtered);
+    if (filtered.length > 0) {
+      setTimeout(() => {
+        setMapCenter(filtered[0].position);
+      }, 500);
+    } else {
+      setMapCenter(center);
+    }
+  };
+
+  console.log("markersData", markers);
   return (
-    <div className="App">
-      <Map />
-    </div>
+    <main className="main_container">
+      <div className="store-list">
+        <div className="heading">
+          <h2>Our Locations</h2>
+        </div>
+        <div style={{ marginLeft: 10, marginRight: "10px" }}>
+          <div
+            style={{
+              display: "flex",
+              marginTop: "10px",
+              marginBottom: "10px",
+              gap: "10px",
+            }}
+          >
+            <input
+              placeholder="Search for branches"
+              type="text"
+              style={{
+                width: "100%",
+                padding: "8px",
+              }}
+              value={searchQuery}
+              onChange={handleSearchChange}
+            />
+            <button onClick={handleSearch}>Search</button>
+          </div>
+        </div>
+
+        {branches &&
+          branches?.map((company) => (
+            <ul className="list">{company?.nameEng}</ul>
+          ))}
+
+        {markers &&
+          markers?.map((item) => (
+            <ul className="list">
+              <li>{item?.country}</li>
+              <li>{item?.name}</li>
+              <li>{item?.email}</li>
+            </ul>
+          ))}
+      </div>
+
+      <Map filteredMarkers={filteredMarkers} mapCenter={mapCenter} />
+    </main>
   );
 }
 
--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,63 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import {
   GoogleMap,
   LoadScript,
   MarkerF,
   InfoWindow,
 } from "@react-google-maps/api";
-import axios from "axios";
-
-const Map = () => {
+const Map = ({ filteredMarkers, mapCenter }) => {
   const [selectedMarker, setSelectedMarker] = useState(null);
-  const [searchQuery, setSearchQuery] = useState("");
-  const [filteredMarkers, setFilteredMarkers] = useState([]);
-  const [branches, setBranches] = useState([]);
 
   const mapContainerStyle = {
-    height: "96vh",
+    height: "100vh",
     width: "100%",
   };
-
-  const center = {
-    lat: 25.2048, //! Default Dubai Lat long
-    lng: 55.2708,
-  };
-
-  const [mapCenter, setMapCenter] = useState(center);
-
-  const getCompanyLocations = (id) => {
-    axios
-      .get(`https://fcssoftwares.com/api/CompanyProfile/GetOfficeByCPID/${id}`)
-      .then((result) => {
-        console.log(result?.data);
-        setBranches(result?.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
-  useEffect(() => {
-    getCompanyLocations(1);
-  }, []);
-
-  const data = branches && branches?.map((item) => item?.offices);
-  const markersData = data && data?.flatMap((item) => item);
-
-  const markers =
-    markersData &&
-    markersData?.map((branch, index) => ({
-      id: index,
-      name: branch?.branches?.address,
-      position: {
-        lat: parseFloat(branch?.branches?.lat),
-        lng: parseFloat(branch?.branches?.long),
-      },
-      description: branch.address,
-      country: branch?.branches?.address,
-    }));
-
-  console.log("markers", markers);
 
   const handleMarkerClick = (marker) => {
     setSelectedMarker(marker);
@@ -67,49 +21,8 @@ const Map = () => {
     setSelectedMarker(null);
   };
 
-  const handleSearchChange = (event) => {
-    setSearchQuery(event.target.value);
-  };
-
-  const handleSearch = (event) => {
-    const filtered = markers.filter((marker) =>
-      marker.country.toLowerCase().includes(searchQuery.toLowerCase())
-    );
-    console.log("filtered", filtered);
-    setFilteredMarkers(filtered);
-    if (filtered.length > 0) {
-      setTimeout(() => {
-        setMapCenter(filtered[0].position);
-      }, 500);
-    } else {
-      setMapCenter(center);
-    }
-  };
-
   return (
     <>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          paddingTop: 20,
-          paddingBottom: 40,
-          gap: 20,
-        }}
-      >
-        <input
-          placeholder="Search for branches"
-          type="text"
-          style={{
-            width: "14%",
-            padding: "4px",
-          }}
-          value={searchQuery}
-          onChange={handleSearchChange}
-        />
-        <button onClick={handleSearch}>Search</button>
-      </div>
       <LoadScript googleMapsApiKey="AIzaSyDU7VCnMXbqJgHGVsoHM4auKTUOpZI4I3Q">
         <GoogleMap
           mapContainerStyle={mapContainerStyle}
